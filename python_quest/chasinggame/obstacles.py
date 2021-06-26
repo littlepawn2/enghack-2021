97719a200e3324e53fb80cdf2391edabacaccb59
bagelcats <86501767+bagelcats@users.noreply.github.com>
2021-06-26 16:07:15 -0400

    enemy and obstacle graphics

--- python_quest/chasinggame/obstacles.py
+++ python_quest/chasinggame/obstacles.py
@@ -8,7 +8,7 @@ class Obstacle(object):
     #soft - slows player
     #hard - stops player
     
-    def __init__(self, posx, posy, lenx, leny, type):
+    def __init__(self, pic, posx, posy, lenx, leny, type):
         self.hitbox = Rectangle(posx, posy, lenx, leny)
         self.posx = posx
         self.posy = posy
@@ -18,6 +18,12 @@ class Obstacle(object):
             raise ValueError("Unsupported obstacle type")
         self.type = type
         
+        self.pic = pic
+        self.x = int(posx)
+        self.y = int(posy)
+        self.pic_lenx = int(self.lenx)
+        self.pic_leny = int(self.leny)
+        
     def getHitbox(self):
         return self.hitbox
     
@@ -26,8 +32,12 @@ class Obstacle(object):
     
     def drawObject(self):
         if self.type == "HARD":
-            fill(255)
+            fill(255, 255, 255, 100)
+            copy(self.pic, 100, 100, 700, 600, self.x, self.y, self.pic_lenx, self.pic_leny)
+            #rect(self.posx, self.posy, self.lenx, self.leny)
+            
         elif self.type == "SOFT":
-            fill(100)
-        rect(self.posx, self.posy, self.lenx, self.leny)
+            fill(0)
+            #rect(self.posx, self.posy, self.lenx, self.leny)
+            copy(self.pic, 0, 0, 1000, 1000, self.x-40, self.y-30, self.pic_lenx *4 + 10, self.pic_leny *5 + 20)
         
