e7b8d4f0af513496f41827043f50566e9a60758d
William Wen <littlepawn2@gmail.com>
2021-06-26 11:00:10 -0400

    setup movable class + constructors

--- python_quest/chasinggame/entities.py
+++ python_quest/chasinggame/entities.py
@@ -1,16 +1,36 @@
+from collision_detectors import Circle
+from collision_detectors import Rectangle
 
 ##file for anything that moves
 
-class Player:
+class Movable:
     
-    def __init__(self):
-        pass
+    MAX_SPEED = 25
+    
+    def __init__(self, posx, posy):
+        self.pos = PVector(posx, posy)
+        self.vel = PVector(0, 0)
+        self.acc = PVector(0, 0)
+        
+    def move():
+        if self.vel.mag() > MAX_SPEED:
+            self.vel.setMag(MAX_SPEED)
+        
+        self.pos.add(vel)
+        self.vel.add(acc)
+
+class Player(Movable):
+    
+    def __init__(self, posx, posy, rad):
+        super().__init__(posx, posy)
+        self.rad = rad
+        self.hitbox = Circle(posx, posy, rad)
         
         
 
 
 
-class Enemy:
+class Enemy(Movable):
     
-    def __init__(self):
+    def __init__(self, posx, posy):
         pass
