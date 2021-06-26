4a016351430a58efc9788ded73bf609cfb33e54e
William Wen <littlepawn2@gmail.com>
2021-06-26 00:56:16 -0400

    class structure setup

---
+++ python_quest/chasinggame/collision_detectors.py
@@ -0,0 +1,35 @@
+
+##different shaped hitboxes
+
+##everything should have a detect and update function
+##detect should take another shape and return true if hit, false if not hit
+##update should take x & y position and update it's own position
+
+class Circle:
+    ##a circular hitbox
+    
+    def __init__(self):
+        pass
+        
+    def detect():
+        pass
+        
+    def update(x, y):
+        pass
+        
+        
+        
+
+
+
+class Rectangle:
+    ##a rectanglar hitbox
+    
+    def __init__(self):
+        pass
+        
+    def detect():
+        pass
+        
+    def update(x, y):
+        pass
---
+++ python_quest/chasinggame/entities.py
@@ -0,0 +1,16 @@
+
+##file for anything that moves
+
+class Player:
+    
+    def __init__(self):
+        pass
+        
+        
+
+
+
+class Enemy:
+    
+    def __init__(self):
+        pass
