84a321535fb750069ef4371f4586cc3fc435dfba
SOSthegamer <86503143+SOSthegamer@users.noreply.github.com>
2021-06-26 15:55:42 -0400

    fixed return + differentiate between rect and cir

--- python_quest/chasinggame/collision_detectors.py
+++ python_quest/chasinggame/collision_detectors.py
@@ -13,35 +13,39 @@ class Circle:
         #returns true if colliding with shape, false if not
         
         #detects circle circle collision
-        if (self.rad/2) + (Enemy.rad/2) > ((self.posx - Enemy.posx)**2 + (self.posy - Enemy.posy)**2)**0.5:
-            self.sh = True
-        elif (self.rad/2) + (Enemy.rad/2) < ((self.posx - Enemy.posx)**2 + (self.posy - Enemy.posy)**2)**0.5:
-            self.sh = False
-        
-        
-        
-        #detects rectangle circle collision    
-        def clamp(num, min_value, max_value):
-            return max(min(num, max_value), min_value)
-        pointrect_x = clamp(self.posx, Rectangle.posx, Rectangle.posx + Rectangle.lenx)
-        pointrect_y = clamp(self.posy, Rectangle.posy, Rectangle.posy + Rectangle.leny)
-        
-        if ((pointrect_x - self.posx)**2 + (pointrect_y - self.posy)**2)**0.5 < self.rad/2:
-            self.sh = True
-        
-        
-            #push back 5 px
-            if pointrect_x < self.posx:
-                self.posx -= 5
-            if pointrect_x > self.posx:
-                self.posx += 5
-            if pointrect_y < self.posy:
-                self.posy -=5
-            if pointrect_y > self.posy:
-                self.posy += 5
+        if isinstance(Circle, sh):
             
-        elif ((pointrect_x - self.posx)**2 + (pointrect_y - self.posy)**2)**0.5 < self.rad/2:
-            self.sh = False
+            if (self.rad/2) + (Enemy.rad/2) > ((self.posx - Enemy.posx)**2 + (self.posy - Enemy.posy)**2)**0.5:
+                return True
+            elif (self.rad/2) + (Enemy.rad/2) < ((self.posx - Enemy.posx)**2 + (self.posy - Enemy.posy)**2)**0.5:
+                return False
+        
+        
+        
+        #detects rectangle circle collision
+        elif isinstance(Rectangle, sh):    
+        
+            def clamp(num, min_value, max_value):
+                return max(min(num, max_value), min_value)
+            pointrect_x = clamp(self.posx, Rectangle.posx, Rectangle.posx + Rectangle.lenx)
+            pointrect_y = clamp(self.posy, Rectangle.posy, Rectangle.posy + Rectangle.leny)
+            
+            if ((pointrect_x - self.posx)**2 + (pointrect_y - self.posy)**2)**0.5 < self.rad/2:
+                return True
+            
+            
+                #push back 5 px
+                if pointrect_x < self.posx:
+                    self.posx -= 5
+                if pointrect_x > self.posx:
+                    self.posx += 5
+                if pointrect_y < self.posy:
+                    self.posy -=5
+                if pointrect_y > self.posy:
+                    self.posy += 5
+                
+            elif ((pointrect_x - self.posx)**2 + (pointrect_y - self.posy)**2)**0.5 < self.rad/2:
+                return False
         
         
     def update(self, x, y):
