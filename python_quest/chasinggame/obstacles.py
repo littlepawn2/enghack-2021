a3ee6b318acbc8a6cf44b697b2b0d3aaa65d928a
William Wen <littlepawn2@gmail.com>
2021-06-26 09:58:40 -0400

    added two more classes

--- python_quest/chasinggame/obstacles.py
+++ python_quest/chasinggame/obstacles.py
@@ -1,2 +1,17 @@
 
 ##file for obstacles
+
+class SoftObstacle:
+    #slows down player while within bounds
+    
+    def __init__(self):
+        pass
+        
+        
+        
+        
+class HardObstacle:
+    #stops player when colliding
+    
+    def __init__(self):
+        pass
